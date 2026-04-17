6133fd7b63f0c0e11725ea375a0069638f9ad761
aparnt2 <aparnabrq@gmail.com>
2026-04-17 04:53:40 -0700

    fix cors allow all origins

--- main.py
+++ main.py
@@ -44,14 +44,14 @@ def startup_event():
 
 
 origins = [
-    "http://localhost:5174"
+   "*"
     
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
